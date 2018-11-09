756cba31d711f2566c92214a8892e8ab9208aeb8
lindsey <linds.nicolewalker@gmail.com>
2018-11-08 17:12:18 -0800

    adding some variables and an array to JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+var wins = 0;
+
+var losses = 0;
+
+var numGuesses = 13;
+
+var guessChoices = [];
