3f9d27d0437b36f72f3c808157daf5bca42deac7
lindsey <linds.nicolewalker@gmail.com>
2018-11-08 17:14:16 -0800

    adding more variables, an event key, math, and another array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,3 +7,12 @@ var losses = 0;
 var numGuesses = 13;
 
 var guessChoices = [];
+
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+    
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+      
