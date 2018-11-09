8ae8d816e256bbc90129482ec194c69e37c38d04
lindsey <linds.nicolewalker@gmail.com>
2018-11-08 17:15:44 -0800

    adding the conditionals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,11 +8,33 @@ var numGuesses = 13;
 
 var guessChoices = [];
 
-document.onkeyup = function(event) {
+    document.onkeyup = function(event) {
 
-    var userGuess = event.key;
+var userGuess = event.key;
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
     
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    
-    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-      
+if (options.indexOf(userGuess) > -1) {
+
+    if (userGuess === computerGuess) {
+        
+        wins++;
+        numGuesses = 13;
+        guessChoices = [];
+    }
+
+    if (userGuess !== computerGuess) {
+
+        numGuesses --;
+        guessChoices.push(userGuess);
+    }
+
+    if (numGuesses === 0) {
+
+            numGuesses = 13;
+            losses ++;
+            guessChoices = [];
+        }
+ 
